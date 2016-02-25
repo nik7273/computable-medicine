0b3f00f149aa37df28ba248df51662ddab056285
Michael Chary <mac389@gmail.com>
2016-02-25 09:19:40 -0500

    update

--- testing_prepositions.py
+++ testing_prepositions.py
@@ -3,25 +3,37 @@ import nltk
 from nltk import Tree
 from nltk.sem import cooper_storage as cs
 
-grammar = nltk.data.load(r'./with.fcfg')
+#grammar = nltk.data.load(r'./with.fcfg')
+#grammar = nltk.data.load(r'./many-quantified-nps.fcfg')
+def parse_cooper_storage(tree):
+	semantic_representation = cs.CooperStore(tree.label()['SEM'])
+	print semantic_representation.s_retrieve,'k'
+	semantic_representation.s_retrieve()
+	for reading in semantic_representation.readings:
+		print 'Reading %s'%reading
+		print 'Simplified Reading %s'%reading.simplify()
+		print 'Predicates: ', reading.predicates()
+		print 'Variables: ',reading.variables()
 
-hpi = "33_year_old man with coronary_artery_disease and stroke" #in 2010
+hpi = "33_year_old man with coronary_artery_disease complicated_by coronary_artery_bypass_graft in 2010"
+#hpi = '33_year_old man'
+#hpi = "stroke and coronary_artery_disease in 2010"
+#hpi = "33_year_old man with coronary_artery_disease and stroke in 2010"
 #hpi = "coronary_artery_disease complicated_by coronary_artery_bypass_graft"
 #parser = nltk.parse.FeatureEarleyChartParser(grammar)
-trees = cs.parse_with_bindops(hpi, grammar=r'./with.fcfg')
+#trees = cs.parse_with_bindops(hpi, grammar=r'./with.fcfg')
+trees = cs.parse_with_bindops(hpi, grammar=r'./many-quantified-nps.fcfg')
+
 #trees = [tree for tree in parser.parse(hpi)]
 if len(trees) == 1:
-	phrase_tree = trees[0]
-	phrase_tree.draw()
-	semrep = trees[0].label()['SEM']
-	cs_semrep = cs.CooperStore(semrep)
-	cs_semrep.s_retrieve() #Must S-retrieve before can get readings. 
-	reading = cs_semrep.readings[0]
+	trees[0].draw()
+	parse_cooper_storage(trees[0])
+	
 else:
 	print '%d trees. Have to choose'%len(trees)
 	print 'No choice function defined'
-	trees[0].draw()
+	
+	for tree in trees:
+		tree.draw()
+		parse_cooper_storage(tree)
 
-print reading
-print reading.predicates()
-print reading.constants()
